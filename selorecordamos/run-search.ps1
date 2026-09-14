7b95f43e22f21aeb75c6bb83bfe09df768e78f60
fabricelop <fabricelop@yahoo.fr>
2026-09-15 00:30:16 +0200

    Fix SeLoRecordamos UTF-8 log display in Windows PowerShell

--- selorecordamos/run-search.ps1
+++ selorecordamos/run-search.ps1
@@ -2,7 +2,7 @@ $ErrorActionPreference = 'Stop'
 $repo = Split-Path -Parent $PSScriptRoot
 Set-Location $repo
 
-$utf8 = New-Object System.Text.UTF8Encoding($false)
+$utf8 = New-Object System.Text.UTF8Encoding($true)
 [Console]::OutputEncoding = $utf8
 $OutputEncoding = $utf8
 
@@ -15,6 +15,23 @@ $env:SR_HEADLESS = '1'
 $runtime = Join-Path $PSScriptRoot 'runtime'
 New-Item -ItemType Directory -Force -Path $runtime | Out-Null
 $log = Join-Path $runtime 'search-task.log'
+
+function Ensure-Utf8BomLog {
+    if (-not (Test-Path $log)) {
+        [System.IO.File]::WriteAllText($log, '', $utf8)
+        return
+    }
+
+    $bytes = [System.IO.File]::ReadAllBytes($log)
+    $hasBom = $bytes.Length -ge 3 -and $bytes[0] -eq 0xEF -and $bytes[1] -eq 0xBB -and $bytes[2] -eq 0xBF
+    if (-not $hasBom) {
+        $legacy = Join-Path $runtime 'search-task-legacy.log'
+        Move-Item -Force $log $legacy
+        [System.IO.File]::WriteAllText($log, '', $utf8)
+    }
+}
+
+Ensure-Utf8BomLog
 
 function Add-LogLine([string]$text) {
     [System.IO.File]::AppendAllText($log, $text + [Environment]::NewLine, $utf8)
@@ -32,6 +49,8 @@ function Run-NodeToLog([string[]]$arguments) {
     $psi.UseShellExecute = $false
     $psi.RedirectStandardOutput = $true
     $psi.RedirectStandardError = $true
+    $psi.StandardOutputEncoding = New-Object System.Text.UTF8Encoding($false)
+    $psi.StandardErrorEncoding = New-Object System.Text.UTF8Encoding($false)
     $psi.CreateNoWindow = $true
     $psi.Arguments = (($arguments | ForEach-Object { Quote-Arg $_ }) -join ' ')
 
